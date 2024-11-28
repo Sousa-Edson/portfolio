9b26845e72828d113467f33df7af5dae4ed8e55b
Edson Sousa <edson3711.es@gmail.com>
2024-11-28 15:48:59 -0300

    feat: ajuste de layout de tecnologias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { TecnologiasComponent } from "./components/tecnologias/tecnologias.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TecnologiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
